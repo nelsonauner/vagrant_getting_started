318180443d69b213197da54af9e547265c1c66a9
Nelson Auner <nelson@nelsonauner.com>
2015-04-09 17:28:00 -0500

    bigmemory support, next todo: vimrc and vim-r

---
+++ R_setup.R
@@ -0,0 +1,6 @@
+#!/usr/bin/Rscript
+cat("Hello, world")
+
+install.packages("bigmemory")
+install.packages("biganalytics")
+install.packages("bigtabulate")
---
+++ vim_setup.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/https://raw.githubusercontent.com/nelsonauner/vim/master/_vimrc
